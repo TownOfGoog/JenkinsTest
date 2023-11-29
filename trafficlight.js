badf10e08807070353f37bbf50ec4ffa4fb7bc54
TownOfGoog <95412264+TownOfGoog@users.noreply.github.com>
2023-11-29 14:03:55 +0100

    Create trafficlight.js

---
+++ trafficlight.js
@@ -0,0 +1,19 @@
+function trafficLight(color){
+    if (color === undefined){//??
+        return `OUT OF ORDER`//??
+    } else {//??
+        if (color === 'red'){//??
+            return `DON'T WALK!`//??
+        }//??
+        else if (color === 'orange'){//??
+            return `ATTENTION!`//??
+        }//??
+        else if (color === 'green'){//??
+            return `WALK!`//??
+        }//??
+    }//??
+}
+
+module.exports = {
+    trafficLight
+}
